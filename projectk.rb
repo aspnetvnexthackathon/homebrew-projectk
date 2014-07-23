f8627700a053750f53074e2997030653c723c82a
Nate McMaster <nate.mcmaster@outlook.com>
2014-07-23 11:14:52 -0700

    Update projectk.rb

--- projectk.rb
+++ projectk.rb
@@ -3,8 +3,8 @@ require "formula"
 class Projectk < Formula
   homepage "https://www.github.com/aspnet/Home"
   version "1.0.0-alpha3-10163"
-  url "https://www.myget.org/F/aspnetvnext/api/v2/package/KRE-mono45-x86/1.0.0-alpha3-10163"
-  sha1 "aafb7e88fdb240fb6b9ca2aa1aeae73ed61da9ac"
+  url "https://www.myget.org/F/aspnetvnext/api/v2/package/KRE-mono45-x86/1.0.0-alpha3-10165"
+  sha1 "e1852585c9457fc1464913796fc2eebbd9f4e8c9"
 
   depends_on "mono" => :recommended
 
